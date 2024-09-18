3e242b9eb86b48e2d0124cdb679f43402f714a70
James Milne <james.milne@redis.com>
2024-09-18 15:01:04 +0300

    add stuff for syntax highlighting + autocomplete

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -167,8 +167,13 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
+        ruff = {},
+        gopls = {},
+        terraformls = {},
+        dockerls = {},
+        jsonls = {},
+        yamlls = {},
+        ts_ls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs

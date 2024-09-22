458482a756972c911146dbb783e0248669d9a11b
James Milne <jmilne22@gmail.com>
2024-09-22 21:20:17 +0300

    Add new keybindings for git, disable arrows

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,3 +1,11 @@
 return {
   'tpope/vim-fugitive',
+  config = function()
+    -- This example maps :G to <leader>g in normal mode
+    vim.api.nvim_set_keymap('n', '<leader>gs', ':Git<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gc', ':Git commit<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gp', ':Git push<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gl', ':Git pull<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>ga', ':Git add .<CR>', { noremap = true, silent = true })
+  end,
 }
